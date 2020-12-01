674d89611acff9503bedd68122c23174f85b7db8
sebbyyys <sebaszwed@outlook.com>
2020-12-01 16:25:06 +0000

    Update 1

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+	{
+		title: 'Home',
+		url: '#',
+		cName: 'nav-links'
+	},
+	{
+		title: 'Dashboard',
+		url: '#',
+		cName: 'nav-links'
+	},
+	{
+		title: 'Exercises',
+		url: '#',
+		cName: 'nav-links'
+	},
+	{
+		title: 'Rep Counter',
+		url: '#',
+		cName: 'nav-links'
+	},
+	{
+		title: 'Sign Up',
+		url: '#',
+		cName: 'nav-links-sign-up'
+	},
+	
+]
